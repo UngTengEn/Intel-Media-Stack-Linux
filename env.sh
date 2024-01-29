4495f12e2f74ec0660360ca498b383b45897ab9c
Ung, Teng En <teng.en.ung@intel.com>
2024-01-29 15:36:20 +0800

    Add submodules for 2023Q4 release

--- env.sh
+++ env.sh
@@ -13,6 +13,7 @@ else
 fi
 
 if [ -z $(echo $LD_LIBRARY_PATH | grep "$InstallDir") ]; then
+  [ -z $INTEL_MEDIA_STACK_PATH ] && export INTEL_MEDIA_STACK_PATH=$InstallDir
 	export PATH=$InstallDir/bin:$PATH
 	export LD_LIBRARY_PATH=$InstallDir/lib64
 	export PKG_CONFIG_PATH=$InstallDir/lib64/pkgconfig
