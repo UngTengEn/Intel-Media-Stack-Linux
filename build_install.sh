d4dc9e92607b8250d2b9cd06472fb800e53140af
Ung, Teng En <teng.en.ung@intel.com>
2024-01-30 16:37:04 +0800

    Build and install on local _install folder

--- build_install.sh
+++ build_install.sh
@@ -9,23 +9,23 @@ MesonDebugFlags=""
 if [ "$1" == "debug" ]; then
 	BuildType="Debug"
 	buildtype="debug"
-	CmakeDebugFlags="-DCMAKE_CXX_FLAGS=-fsanitize=address -DCMAKE_C_FLAGS=-fsanitize=address"
-	MesonDebugFlags="-Db_sanitize=address"    
+#	CmakeDebugFlags="-DCMAKE_CXX_FLAGS=-fsanitize=address -DCMAKE_C_FLAGS=-fsanitize=address"
+#	MesonDebugFlags="-Db_sanitize=address"    
 fi
+
 NPROC=$(nproc)
 [ $NPROC -gt 4 ] && NPROC=$(( $NPROC - 2 ))
 
-
 function meson_install() {
 	if [ ! -e _build/$BuildType/$1/build.ninja ]; then
-    meson setup _build/$BuildType/$1 $1 --prefix="$INTEL_MEDIA_STACK_PATH" --libdir="lib64" --buildtype "$buildtype" ${MesonDebugFlags}
+    meson setup _build/$BuildType/$1 $1 --prefix="$PWD/_install" --libdir="lib64" --buildtype "$buildtype" ${MesonDebugFlags}
   fi
   ninja -C _build/$BuildType/$1 install
 }
 
 function cmake_install() {
 	if [ ! -f _build/$BuildType/$1/Makefile ]; then
-    cmake -S $1 -B _build/$BuildType/$1 -DCMAKE_INSTALL_PREFIX="$INTEL_MEDIA_STACK_PATH" -DCMAKE_INSTALL_LIBDIR="lib64" -DCMAKE_BUILD_TYPE="$BuildType" $2
+    cmake -S $1 -B _build/$BuildType/$1 -DCMAKE_INSTALL_PREFIX="$PWD/_install" -DCMAKE_INSTALL_LIBDIR="lib64" -DCMAKE_BUILD_TYPE="$BuildType" $2
   fi
   make -j$NPROC -C _build/$BuildType/$1 install
 }
--- env.sh
+++ env.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
 CurDir=$(pwd)
-InstallDir=""
-
-if [ ! -z $INTEL_MEDIA_STACK_PATH ]; then
-	InstallDir="$INTEL_MEDIA_STACK_PATH"
-elif [ ! -z $(echo $CurDir | grep "\/src\/") ]; then
-	InstallDir=$(echo $CurDir | sed 's/\/src\/.*//g')
-else
-	echo "Please set the INTEL_MEDIA_STACK_PATH environment or the stack should be in \"*/src/*\" path."
-	exit 1
-fi
+InstallDir="$PWD/_install"
 
 if [ -z $(echo $LD_LIBRARY_PATH | grep "$InstallDir") ]; then
-  [ -z $INTEL_MEDIA_STACK_PATH ] && export INTEL_MEDIA_STACK_PATH=$InstallDir
 	export PATH=$InstallDir/bin:$PATH
 	export LD_LIBRARY_PATH=$InstallDir/lib64
 	export PKG_CONFIG_PATH=$InstallDir/lib64/pkgconfig
