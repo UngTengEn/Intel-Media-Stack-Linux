c3e9992d66f3fe0c78447da13f7f54015d9eb276
Ung, Teng En <teng.en.ung@intel.com>
2024-06-25 14:43:23 +0800

    Always enable x11 on libva

--- build.sh
+++ build.sh
@@ -20,21 +20,21 @@ NPROC=$(nproc)
 [ $NPROC -gt 4 ] && NPROC=$(( $NPROC - 2 ))
 
 function meson_install() {
-	if [ ! -e _build/$BuildType/$1/build.ninja ]; then
-    meson setup _build/$BuildType/$1 $1 --prefix="$PWD/_install" --libdir="lib64" --buildtype "$buildtype" ${MesonDebugFlags}
+  if [ ! -e _build/$BuildType/$1/build.ninja ]; then
+    meson setup _build/$BuildType/$1 $1 --prefix="$PWD/_install" --libdir="lib64" --buildtype "$buildtype" $2 ${MesonDebugFlags}
   fi
   ninja -C _build/$BuildType/$1 install
 }
 
 function cmake_install() {
-	if [ ! -f _build/$BuildType/$1/Makefile ]; then
+  if [ ! -f _build/$BuildType/$1/Makefile ]; then
     cmake -S $1 -B _build/$BuildType/$1 -DCMAKE_INSTALL_PREFIX="$PWD/_install" -DCMAKE_INSTALL_LIBDIR="lib64" -DCMAKE_BUILD_TYPE="$BuildType" $2
   fi
   make -j$NPROC -C _build/$BuildType/$1 install
 }
 
 if [ "$BuildComponents" == "all" ] || [ "$BuildComponents" == "vaapi" ]; then
-	meson_install libva
+	meson_install libva "-Dwith_x11=yes"
 	meson_install libva-utils
 fi
 
